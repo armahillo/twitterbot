821358c1681647eb068ee8804549973fd9cbefae
Aaron Hill <armahillo@gmail.com>
2016-01-24 14:46:08 -0500

    Converts config to use dotenv

--- config/twitterbot_config.rb
+++ config/twitterbot_config.rb
@@ -2,14 +2,14 @@
 
 # API info for Twitter
 TWITTER_API_SETTINGS = {
-  :consumer_key => "",
-  :consumer_secret => "",
-  :access_token => "",
-  :access_token_secret => ""
+  :consumer_key => ENV["CONSUMER_KEY"],
+  :consumer_secret => ENV["CONSUMER_SECRET"],
+  :access_token => ENV["ACCESS_TOKEN"],
+  :access_token_secret => ENV["ACCESS_TOKEN_SECRET"]
 }
 
 TWITTERBOT_OPTIONS = {
-  :profile_name => "Failbot",
-  :search_tag => "#fail",
-  :block_file => "config/blocked_userids.json"
+  :profile_name => ENV["PROFILE_NAME"],
+  :search_tag => ENV["SEARCH_TAG"],
+  :block_file => ENV["BLOCK_FILE_PATH"]
 }
